f74c5523fd75be16c1d04c78d3e9190aae0a79ed
Shahzod_Sharofiddinov <sharofiddinovshahzod3@gmail.com>
2023-02-08 14:49:44 +0500

    add

---
+++ app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', Home.as_view(), name='home'),
+    path('about/', About.as_view(), name='about'),
+    path('contact/', Contact.as_view(), name='contact'),
+    path('detail/<int:pk>/', Detail.as_view(), name='detail'),
+    path('course/', Course.as_view(), name='course'),
+    path('team/', Team.as_view(), name='team'),
+    path('testimonial/', Testimonial.as_view(), name='testimonial'),
+    path('feature/', Feature.as_view(), name='feature'),
+]
